2468e482519a7ea20eff7bbef793780a0b03cd8a
Jy <84299531+Jy738@users.noreply.github.com>
2024-07-08 23:50:11 -0500

    Update client.js

--- client.js
+++ client.js
@@ -1,2 +1,2 @@
 var bwcontext = $("#bwxp");
-bwcontext.load('https://corsproxy.io/?' + encodeURIComponent("https://620ee524-ec28-4c0c-84a9-8a891f8a0687-00-2yl8bpx3ypd0p.janeway.replit.dev/");
+bwcontext.load('https://corsproxy.io/?' + encodeURIComponent("https://620ee524-ec28-4c0c-84a9-8a891f8a0687-00-2yl8bpx3ypd0p.janeway.replit.dev/"));
